0f521b995ff0d85040ab9411f72853225d6f808c
lpg <1003289700@qq.com>
2017-11-09 18:01:54 +0800

    处理请求

--- app.js
+++ app.js
@@ -1,60 +1,22 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-// 创建一个Koa对象表示web app本身:
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
 const app = new Koa();
 
-// 对于任何请求，app将调用该异步函数处理请求：
-/*app.use(async (ctx, next) => {
+// log request URL:
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
   await next();
-  // 设置response的Content-Type:
-  ctx.response.type = 'text/html';
-  // 设置response的内容:
-  ctx.response.body = '<h1>Hello, koa2!</h1>';
-});*/
-
-app.use(async (ctx, next) => {
-  console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-  await next(); // 调用下一个middleware
 });
 
-app.use(async (ctx, next) => {
-  const start = new Date().getTime(); // 当前时间
-  await next(); // 调用下一个middleware
-  const ms = new Date().getTime() - start; // 耗费时间
-  console.log(`Time: ${ms}ms`); // 打印耗费时间
-});
+// parse request body:
+app.use(bodyParser());
 
-app.use(async (ctx, next) => {
-  if (ctx.request.path === '/') {
-    ctx.response.body = 'index page';
-  } else {
-    await next();
-  }
-});
+// add controllers:
+app.use(controller());
 
-app.use(async (ctx, next) => {
-  if (ctx.request.path === '/test') {
-    ctx.response.body = 'TEST page';
-  } else {
-    await next();
-  }
-});
-
-app.use(async (ctx, next) => {
-  if (ctx.request.path === '/error') {
-    ctx.response.body = 'ERROR page';
-  } else {
-    await next();
-  }
-});
-
-app.use(async (ctx, next) => {
-  await next();
-  ctx.response.type = 'text/html';
-  ctx.response.body = '<h1>Hello, koa21!</h1>';
-});
-
-// 在端口3000监听:
 app.listen(3000);
 console.log('app started at port 3000...');
